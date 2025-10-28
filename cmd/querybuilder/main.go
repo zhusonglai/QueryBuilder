d02b61549d2e8d8fdeef9149323d1e32060c667e
Zhu <zhusonglai@Live.cn>
2025-10-28 22:19:19 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/querybuilder/main.go
@@ -0,0 +1,23 @@
+// cmd/querybuilder/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "querybuilder/internal/querybuilder"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := querybuilder.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
